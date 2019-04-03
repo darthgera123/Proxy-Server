baa33559f559f4f083ac1c3e5a65ee3f71a2b032
anant_1234 <anantagarwal2426@gmail.com>
2019-04-03 15:46:28 +0530

    Errors corrected

--- proxy.py
+++ proxy.py
@@ -5,22 +5,25 @@ import sys
 
 PROXY_PORT = 20100
 MAX_CONN = 10
-BUFFER_SIZE = 2048000
+BUFFER_SIZE = 100000000
 
 def handle_parsing(client_socket, c_addr, data):
 	tdata = data	
 	data = str(data)
 	fline = data.split('\n')[0]
-	url = fline.split(' ')[1]
+	try:
+		url = fline.split(' ')[1]
+	except:
+		sys.exit()
 	print(url)
 
-	http_pos = url.find("://") # find pos of ://
+	http_pos = url.find("://") 
 	if (http_pos==-1):
 	    temp = url
 	else:
-	    temp = url[(http_pos+3):] # get the rest of url
+	    temp = url[(http_pos+3):] 
 
-	port_pos = temp.find(":") # find the port pos (if any)
+	port_pos = temp.find(":") 
 
 	# find end of web server
 	webserver_pos = temp.find("/")
@@ -39,35 +42,36 @@ def handle_parsing(client_socket, c_addr, data):
 	    port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
 	    webserver = temp[:port_pos] 	
 
-	print(webserver)
-	print(port)
+	# print(webserver)
+	# print(port)
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 	s.settimeout(60)
-	s.connect((webserver, port))
-	s.sendall(tdata)
+	try:
+		s.connect((webserver, port))
+		s.sendall(tdata)
+	except:
+		sys.exit()
 
 	while 1:
-    
-	    temp_data = s.recv(BUFFER_SIZE)
-	    if (len(temp_data) > 0):
-	        client_socket.send(temp_data) 
-	    else:
-	        break   
+		try:
+		    temp_data = s.recv(BUFFER_SIZE)
+		    if (len(temp_data) > 0):
+		        client_socket.send(temp_data) 
+		    else:
+		        break 
+		except:
+			sys.exit()
+	sys.exit()  
 
 
 def initiate_server():
-	try:
-		proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		proxy_socket.bind(('127.0.0.1', PROXY_PORT))
-		proxy_socket.listen(MAX_CONN)
-		proxy_ip = proxy_socket.getsockname()[0]
-		print ("Proxy server running on ip address " + str(proxy_ip) + " and port number = " + str(proxy_socket.getsockname()[1]))
-	except Exception as e: 
-		print(e)
-		# When specified address is already in use
-		proxy_socket.close()
-		sys.exit()
+	proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	proxy_socket.bind(('127.0.0.1', PROXY_PORT))
+	proxy_socket.listen(MAX_CONN)
+	proxy_ip = proxy_socket.getsockname()[0]
+	print ("Proxy server running on ip address " + str(proxy_ip) + " and port number = " + str(proxy_socket.getsockname()[1]))
+	
 
 
     # Listening and Accepting requests
@@ -76,6 +80,7 @@ def initiate_server():
 			new_socket, c_addr = proxy_socket.accept()
 			data = new_socket.recv(BUFFER_SIZE)
 			t = threading.Thread(target=handle_parsing, args=(new_socket, c_addr, data))
+			t.setDaemon(True)
 			t.start()
 		except KeyboardInterrupt:
 			proxy_socket.close()
